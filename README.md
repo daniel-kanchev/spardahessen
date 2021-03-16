3d576b9b54538178bca491dfe905397b7c184de7
daniel-kanchev <daniel.kanchev@adata.pro>
2021-03-16 12:21:33 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,11 @@
+URL: https://www.sparda-hessen.de/aktuelle-infos-magazin/magazin.html
+
+    Spider name: spardahessen
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Notes:
---
+++ spardahessen/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'spardahessen'
+SPIDER_MODULES = ['spardahessen.spiders']
+NEWSPIDER_MODULE = 'spardahessen.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'spardahessen.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
